74e9ef3ccd825bb32e9df7d2cd1617a6ce775be7
frederico-lavrador <frederico.lavrador@gmail.com>
2024-03-07 14:41:42 +0000

    feat: updated Error page

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,9 @@ import { router } from './routes.jsx'
 import './index.css'
 import './pages/Home/home.css'
 import './pages/About/about.css'
+import './pages/Projects/projects.css';
 import './pages/Contact/contact.css'
-import './pages/Projects/projects.css'
+import './pages/Error/error.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -5,16 +5,13 @@ export function Error() {
     const location = useLocation();
 
     return (
-        <>
-            <div className="error">
-                <div className="container">
-                    <div className="error__wrapper">
-                        <h1>Something went wrong!</h1>
-                        <hr />
-                        <h2>404 - {location.pathname} not found!</h2>
-                    </div>
+        <section id='error' className='error'>
+            <div className="container">
+                 <div className="error__wrapper">
+                    <h1>Something went wrong!</h1>
+                    <h2>404 - {location.pathname} not found!</h2>
                 </div>
             </div>
-        </>
+        </section>
     )
 }
