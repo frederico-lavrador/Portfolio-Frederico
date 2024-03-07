fa6bd7682212b9d5c94ca8be886d06ebd7641d75
frederico-lavrador <frederico.lavrador@gmail.com>
2024-03-07 14:57:46 +0000

    feat: Created the Projects page

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,8 +1,17 @@
 export function Projects() {
 
     return (
-        <>
-            aaa
-        </>
+        <section id='projects' className='projects'>
+            <div className="container">
+                <div className="projects__wrapper">
+                    <div className="projects__title">
+                        <h2>These are the projects I've worked on: </h2>
+                    </div>
+                    <div className="projects__projects">
+                        <h3>Coming Soon!</h3>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
