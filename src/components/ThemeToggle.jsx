6dd420dde5051e9d07594d347f60b810f1f0787f
frederico-lavrador <frederico.lavrador@gmail.com>
2024-03-08 12:46:11 +0000

    feat: created light/dark theme CSS

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -1,12 +1,19 @@
+import { useTheme } from '../contexts/ThemeContext'
+
 function ThemeToggle() {
 
-    return (
-			<div className='header__LightDarkBtn'>
-				<button>
+	const { darkMode, toggleTheme } = useTheme(); 
+
+	return (
+		<div className='header__LightDarkBtn'>
+			<button onClick={toggleTheme}>
+				{darkMode ?
 					<i className='fa fa-sun-o' aria-hidden='true'></i>
-				</button>
-			</div>
-		);
+					:
+					<i className='fa fa-moon-o' aria-hidden='true'></i>}
+			</button>
+		</div>
+	);
 
 }
 
