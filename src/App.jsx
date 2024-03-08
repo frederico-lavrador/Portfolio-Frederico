8326c7bced67c8362709d3cd8cc19150082ecc12
frederico-lavrador <frederico.lavrador@gmail.com>
2024-03-08 20:28:48 +0000

    feat: Added Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import { ThemeProvider } from './contexts/ThemeContext'
 
 function App() {
@@ -7,7 +8,8 @@ function App() {
 		<>
 			<ThemeProvider>
 				<Header/>
-				<Outlet/>
+				<Outlet />
+				<Footer />
 			</ThemeProvider>
 		</>
 	);
