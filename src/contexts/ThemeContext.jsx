bf03d38b4244e831da62d1595da6fb9e2d0db47d
frederico-lavrador <frederico.lavrador@gmail.com>
2024-03-08 11:56:23 +0000

    feat: created context for Dark Theme

---
+++ src/contexts/ThemeContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useState, useContext } from 'react'
+
+const ThemeContext = createContext();
+export const useTheme = () => useContext(ThemeContext);
+export const ThemeProvider = ({ children }) => {
+
+    const [darkMode, setDarkMode] = useState(false);
+    const toggleTheme = () => {
+
+        setDarkMode(prevMode => !prevMode);
+
+    };
+
+    return (
+        <ThemeContext.Provider value={{ darkMode, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+
+};
